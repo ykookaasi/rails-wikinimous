6b9cf364c92585edd509bc637785e97d1814768b
Yoko Kasai <ykookaasi@gmail.com>
2020-11-10 15:59:44 +0100

    Build CRUD for articles app

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def create
+    article = Article.new(article_params)
+    article.save
+
+    redirect_to article_path(article)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit; end
+
+  def show; end
+
+  def update
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+
+    redirect_to article_path
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article = Article.new(
+    title:   Faker::TvShows::Simpsons.quote,
+    content: Faker::Hipster.paragraphs
+  )
+  article.save!
+end
